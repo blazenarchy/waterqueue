417807eec17398e04d2a82ef8f6b3a70a0dd6f2a
willem@leit.so <wnuke@wnuke.dev>
2020-10-18 23:37:03 -0700

    remove unused imports

--- src/main/java/dev/wnuke/waterqueue/WaterqueueCompanion.java
+++ src/main/java/dev/wnuke/waterqueue/WaterqueueCompanion.java
@@ -1,6 +1,5 @@
 package dev.wnuke.waterqueue;
 
-import me.clip.placeholderapi.PlaceholderAPI;
 import org.bukkit.Difficulty;
 import org.bukkit.GameMode;
 import org.bukkit.World;
@@ -9,7 +8,6 @@ import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.PlayerDeathEvent;
 import org.bukkit.event.player.*;
-import org.bukkit.event.server.BroadcastMessageEvent;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.bukkit.plugin.messaging.PluginMessageListener;
 
